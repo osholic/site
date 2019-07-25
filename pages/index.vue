7235c19806127d062b986d0746fe261b02c556e9
liqingwei <liqingwei@shimo.im>
2019-07-25 11:13:11 +0800

    fix: ssr component

--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,12 @@
             <div class="column is-6 is-5-fullhd is-offset-1-fullhd">
               <h1 class="title has-text-grey-dark">让开源更美好</h1>
               <h2 class="desc has-text-grey-light">精美的设计，正版授权的开源周边。在这里，你总能找到一款心仪的商品。</h2>
-              <a
-                class="button shop-btn is-primary is-medium is-rounded"
-                :href="isWeixin ? $themeConfig.links.wxtotaobao : $themeConfig.links.taobao"
-              >进入淘宝店铺</a>
+              <ClientOnly>
+                <a
+                  class="button shop-btn is-primary is-medium is-rounded"
+                  :href="isWeixin ? $themeConfig.links.wxtotaobao : $themeConfig.links.taobao"
+                >进入淘宝店铺</a>
+              </ClientOnly>
             </div>
             <div class="column is-6">
               <img
@@ -91,10 +93,12 @@
     <section class="section shopping">
       <div class="container has-text-centered">
         <h3 class="title shop-title has-text-grey-dark">开始购物吧~</h3>
-        <a
-          class="button is-primary is-medium is-rounded"
-          :href="isWeixin ? $themeConfig.links.wxtotaobao : $themeConfig.links.taobao"
-        >淘宝店铺</a>
+        <ClientOnly>
+          <a
+            class="button is-primary is-medium is-rounded"
+            :href="isWeixin ? $themeConfig.links.wxtotaobao : $themeConfig.links.taobao"
+          >淘宝店铺</a>
+        </ClientOnly>
       </div>
     </section>
   </div>
@@ -126,114 +130,91 @@ export default {
 
 
 <style lang="stylus" scoped>
-.section:nth-of-type(2n) {
-  background: #FAF8F4;
-}
+.section:nth-of-type(2n)
+  background #FAF8F4
 
-.jumbotron {
-  @media (min-width: 768px) {
-    width: 75vw;
-    max-width: unset;
-  }
-}
+.jumbotron
+  @media (min-width: 768px)
+    width 75vw
+    max-width unset
 
-.title {
-  font-weight: bold;
-  font-size: 52px;
-  color: #333;
-  margin-top: 80px;
+.title
+  font-weight bold
+  font-size 52px
+  color #333
+  margin-top 80px
 
-  @media (max-width: 600px) {
-    font-size: 40px;
-    margin-top: 0;
-  }
-}
+  @media (max-width: 600px)
+    font-size 40px
+    margin-top 0
 
-.desc {
-  color: #747474;
-  font-size: 20px;
-  line-height: 1.8;
-}
+.desc
+  color #747474
+  font-size 20px
+  line-height 1.8
 
-.shop-btn {
-  margin-top: 100px;
+.shop-btn
+  margin-top 100px
 
-  @media (max-width: 600px) {
-    margin-top: 40px;
-  }
-}
+  @media (max-width: 600px)
+    margin-top 40px
 
-.features {
-  max-width: 800px;
-  flex-grow: 1;
-  margin: 0 auto;
-  position: relative;
-  width: auto;
+.features
+  max-width 800px
+  flex-grow 1
+  margin 0 auto
+  position relative
+  width auto
 
-  img {
-    width: 90px;
-  }
+  img
+    width 90px
 
-  .feature {
-    transition: 0.3s;
-    border-radius: 24px;
-    padding: 50px 10px;
+  .feature
+    transition 0.3s
+    border-radius 24px
+    padding 50px 10px
 
-    &:hover {
-      transform: translateY(-5px);
-      box-shadow: 0 0 70px rgba(0, 0, 0, 0.06);
-      background-color: #fefcf8;
-    }
+    &:hover
+      transform translateY(-5px)
+      box-shadow 0 0 70px rgba(0, 0, 0, 0.06)
+      background-color #fefcf8
 
-    &:not(:last-of-type) {
-      margin-bottom: 40px;
-    }
-  }
-}
+    &:not(:last-of-type)
+      margin-bottom 40px
 
-.reverse {
-  flex-direction: row-reverse;
+.reverse
+  flex-direction row-reverse
 
-  .text {
-    @media (min-width: 768px) {
-      padding-left: 30px;
-    }
-  }
-}
+  .text
+    @media (min-width: 768px)
+      padding-left 30px
 
-.shop-title {
-  font-size: 40px;
-  margin-top: 0;
-}
+.shop-title
+  font-size 40px
+  margin-top 0
 
-.contact {
-  max-width: 1000px;
-  flex-grow: 1;
-  margin: 0 auto;
-  position: relative;
-  width: auto;
-}
+.contact
+  max-width 1000px
+  flex-grow 1
+  margin 0 auto
+  position relative
+  width auto
 
-.features, .contact {
-  .title {
-    margin-top: 0;
-    font-weight: 600;
-    line-height: 1.125;
-    font-size: 24px;
-  }
+.features, .contact
+  .title
+    margin-top 0
+    font-weight 600
+    line-height 1.125
+    font-size 24px
 
-  .subtitle {
-    font-size: 16px;
-    line-height: 28px;
-    padding-top: 10px;
-  }
-}
+  .subtitle
+    font-size 16px
+    line-height 28px
+    padding-top 10px
 
-.shopping {
-  padding: 6rem 1.5rem;
+.shopping
+  padding 6rem 1.5rem
 
-  .shop-title {
-    margin-bottom: 4rem;
-  }
-}
+  .shop-title
+    margin-bottom 4rem
 </style>
